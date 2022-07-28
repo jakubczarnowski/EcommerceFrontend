0d1f5ac3c7abb5ad16e7aa4fbd7856986083a922
Jakub Czarnowski <czarnowskijakub5@gmail.com>
2022-07-28 20:27:15 +0200

    Revert "Reverted identity changes, category filters"
    This reverts commit 0db6781c3aa4e1568f3f07f2d642a01c08854e81.

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './pages/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@emotion/react';
-import theme from './theme';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
