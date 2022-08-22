f5c85c5f3b5e6e8199bbb3a012dd677ceef62b9c
Jakub Czarnowski <czarnowskijakub5@gmail.com>
2022-08-22 20:03:16 +0200

    Cart working, more changes to the front page and product structure

---
+++ src/utils/CalculateTotalCost.ts
@@ -0,0 +1,9 @@
+import CartItemI from "../types/CartItemI";
+
+export const CalculateTotalCost = (cartItems: CartItemI[]) => {
+	let totalCost = 0;
+	cartItems.forEach((cartItem) => {
+		totalCost += cartItem.product.price * cartItem.quantity;
+	});
+	return totalCost;
+};
