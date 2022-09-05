84a9cae5ae89a0d2ee6e41b6fa4b9209a84866cb
Jakub Czarnowski <58054635+jakubczarnowski@users.noreply.github.com>
2022-09-05 16:22:19 +0200

    working stripe

--- src/components/checkout/Payment.tsx
+++ src/components/checkout/Payment.tsx
@@ -1,16 +1,23 @@
 import { Avatar, Box, Button, Paper, Typography } from "@mui/material";
 import axiosInstance from "../../app/axiosInstance";
 import { BASE_URL } from "../../utils/BaseUrl";
+import StripeCheckout from "react-stripe-checkout";
+import { useEffect, useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
+const stripePromise = loadStripe("pk_test_51LdCHZBKEPMX5hknwI5Qw9m0NPkGmMSNcmn8fVSmyPsbOuxPH41Z4KZHJcS4KNcQRg8syjv3NQzRDlA5RTI8Qg2a00sckIHYAK");
 const Payment = () => {
-	const initiatePay = async () => {
-		const response = await axiosInstance.post("payment/charge", "", {
-			headers: {
-				token: "pk_test_51LdCHZBKEPMX5hknwI5Qw9m0NPkGmMSNcmn8fVSmyPsbOuxPH41Z4KZHJcS4KNcQRg8syjv3NQzRDlA5RTI8Qg2a00sckIHYAK",
-				amount: "30",
-			},
-		});
-		console.log(response);
+	const [clientSecret, setClientSecret] = useState("");
+
+	useEffect(() => {
+		// Create PaymentIntent as soon as the page loads
+		axiosInstance.post("/payment/charge", JSON.stringify({ items: [{ id: "xl-tshirt" }] })).then((data) => setClientSecret(data.data));
+	}, []);
+
+	const options = {
+		clientSecret,
 	};
 	return (
 		<Box style={{ display: "flex", flexDirection: "column", marginTop: "10px" }}>
@@ -20,9 +27,11 @@ const Payment = () => {
 					<Typography variant="body1" sx={{ fontSize: "20px", fontWeight: "400" }}>
 						Order And Pay
 					</Typography>
-					<Button onClick={initiatePay} color="secondary">
-						DWILUQHDWQ
-					</Button>
+					{clientSecret && (
+						<Elements options={options} stripe={stripePromise}>
+							<CheckoutForm />
+						</Elements>
+					)}
 				</Box>
 			</Paper>
 		</Box>
