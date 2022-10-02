f090822e580fbeb0fcab49a76fea03809abfb83f
Jakub Czarnowski <58054635+jakubczarnowski@users.noreply.github.com>
2022-10-02 23:14:30 +0200

    fix

--- src/utils/BaseImageUrl.ts
+++ src/utils/BaseImageUrl.ts
@@ -1,1 +1,1 @@
-export const BASE_IMAGE_URL: string = "https://ecommercebackend.czarnowskijakub.tech/api/v1/static";
+export const BASE_IMAGE_URL: string = "https://ecommercebackend.czarnowskijakub.tech/api/v1/static/";
