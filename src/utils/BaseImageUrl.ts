d3e898cc02a25a6df6d84b0658ef7a3086e6e98e
Jakub Czarnowski <czarnowskijakub5@gmail.com>
2022-10-02 03:11:00 +0200

    url change

--- src/utils/BaseImageUrl.ts
+++ src/utils/BaseImageUrl.ts
@@ -1,1 +1,1 @@
-export const BASE_IMAGE_URL: string = "http://138.2.161.151:8080/api/v1/static/";
+export const BASE_IMAGE_URL: string = "http://138.2.161.151/api/v1/static/";
--- src/utils/BaseUrl.ts
+++ src/utils/BaseUrl.ts
@@ -1,1 +1,1 @@
-export const BASE_URL: string = "http://138.2.161.151:8080/api/v1/";
+export const BASE_URL: string = "http://138.2.161.151/api/v1/";
