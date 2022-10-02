1a0113f2ee1c29e1304c57b7a4d162a35839ca07
Jakub Czarnowski <58054635+jakubczarnowski@users.noreply.github.com>
2022-10-03 01:05:13 +0200

    few changes

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -5,13 +5,17 @@ import { useParams } from "react-router";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import productsSlice, { fetchProductBySlug, selectProductBySlug, selectProducts } from "../reducers/productsSlice";
 import ProductI from "../types/ProductI";
-import { IDLE } from "../utils/states";
+import { FULLFILLED, IDLE, LOADING } from "../utils/states";
 import { BASE_URL } from "../utils/BaseUrl";
 import { BASE_IMAGE_URL } from "../utils/BaseImageUrl";
 import { addToCart, changeQuantity, selectCartItemByProductId } from "../reducers/cartSlice";
 import { Remove, Add } from "@mui/icons-material";
 import Review from "../components/Products/Review";
 import ReviewForm from "../components/Products/ReviewForm";
+import { selectCategories, selectCategoryStatus } from "../reducers/categorySlice";
+import ParseCategories from "../utils/ParseCategories";
+import CategoryI from "../types/CategoryI";
+import getCategoryChain from "../utils/GetCategoryChain";
 
 function ProductPage() {
 	const { slug } = useParams();
@@ -19,6 +23,9 @@ function ProductPage() {
 	const [selectedProductUrl, setSelectedProductUrl] = useState(0);
 	const product = useAppSelector((state) => selectProductBySlug(state, slug || ""));
 	const cartItem = useAppSelector((state) => selectCartItemByProductId(state, product?.id || 0));
+	const categories = useAppSelector(selectCategories);
+	const categoriesStatus = useAppSelector(selectCategoryStatus);
+	let categoryChainElement = undefined;
 	const [tabValue, setTabValue] = useState(0);
 	useEffect(() => {
 		dispatch(fetchProductBySlug(slug || ""));
@@ -33,7 +40,28 @@ function ProductPage() {
 	if (product === undefined) {
 		return <p>Loading</p>;
 	}
-	console.log(product);
+	if (categoriesStatus === FULLFILLED) {
+		const parsedCategories = ParseCategories(categories || ({} as CategoryI));
+		const categoryChain = getCategoryChain(parsedCategories, parsedCategories.find((c) => c.id === product?.categoryId) || ({} as CategoryI)).reverse();
+		categoryChainElement = (
+			<Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "10px" }}>
+				{categoryChain.map((val, index) => {
+					if (val.id == 1) {
+						return (
+							<Typography variant="body2" sx={{ fontSize: "12px", fontWeight: "500", marginBottom: "15px" }}>
+								{"Bazar "} {index == categoryChain.length - 1 ? "" : "-->"}
+							</Typography>
+						);
+					}
+					return (
+						<Typography variant="body2" sx={{ fontSize: "12px", fontWeight: "500", marginBottom: "15px" }}>
+							{val.categoryName} {index == categoryChain.length - 1 ? "" : "-->"}
+						</Typography>
+					);
+				})}
+			</Box>
+		);
+	}
 	return (
 		<Box sx={{ width: "100%", height: "auto", position: "sticky", backgroundColor: "primary.main", overflow: "hidden" }}>
 			<Grid container maxWidth="lg" columns={12} sx={{ paddingTop: "24px", marginBottom: "16px", marginX: " auto" }}>
@@ -57,6 +85,7 @@ function ProductPage() {
 					<Typography variant="h1" sx={{ fontSize: "30px", fontWeight: "600", marginBottom: "20px" }}>
 						{product.name}
 					</Typography>
+					{categoryChainElement !== undefined ? categoryChainElement : <p>...</p>}
 					<Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "20px" }}>
 						<span>Ratings:</span>
 						<Rating value={product.rating} readOnly size="small" sx={{ marginX: "5px" }} precision={0.25} />
